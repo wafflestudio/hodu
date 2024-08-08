6ee22ad872c2c6b476336d11a6595664a64e19a2
Minkyu Lee <whizkyu@snu.ac.kr>
2024-08-09 00:19:25 +0900

    refact: serde 속성 사용하여 응답 스키마 생성 로직 단순화

--- hodu-server/src/api/judge/error.rs
+++ hodu-server/src/api/judge/error.rs
@@ -1,28 +1,30 @@
 use actix_web::{http::StatusCode, HttpResponse, ResponseError};
 use serde::Serialize;
 
-#[derive(thiserror::Error, Debug)]
+#[derive(thiserror::Error, Debug, Serialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE", tag = "detail")]
 pub enum JudgeError {
     #[error("PayloadParseError: {0}")]
-    PayloadParseError(#[source] actix_web::Error),
-}
-
-#[derive(Serialize, Debug)]
-struct JudgeErrorResponse {
-    detail: String,
+    PayloadParseError(
+        #[source]
+        #[serde(skip)]
+        actix_web::Error,
+    ),
+    #[error("HoduCoreError")]
+    HoduCoreError,
 }
 
 impl ResponseError for JudgeError {
     fn status_code(&self) -> StatusCode {
         match self {
             JudgeError::PayloadParseError(_) => StatusCode::BAD_REQUEST,
+            JudgeError::HoduCoreError => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
 
     fn error_response(&self) -> HttpResponse {
         let status_code = self.status_code();
-        let detail = self.to_string();
-        let error_response = JudgeErrorResponse { detail };
+        let error_response = self;
         HttpResponse::build(status_code).json(error_response)
     }
 }
