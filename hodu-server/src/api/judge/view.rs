714399d9d9c19790001778a1077811182d6bb72a
Minkyu Lee <whizkyu@snu.ac.kr>
2024-08-09 23:10:02 +0900

    feat: 동시 채점 수 제한

--- hodu-server/src/api/judge/view.rs
+++ hodu-server/src/api/judge/view.rs
@@ -1,18 +1,24 @@
-use std::panic::AssertUnwindSafe;
+use std::{panic::AssertUnwindSafe, sync::atomic::Ordering, time::Duration};
 
 use actix_web::{post, web, Responder};
 use hodu_core::{mark, MarkParams};
 
 use futures::FutureExt;
 
-use crate::api::judge::{
-    error::JudgeError,
-    schema::{CodeSubmission, MarkResponse},
+use crate::{
+    api::judge::{
+        error::JudgeError,
+        schema::{CodeSubmission, MarkResponse},
+    },
+    MarkCounter,
 };
+
+const MAX_SUBMISSIONS: u32 = 20;
 
 #[post("/submit")]
 async fn submit_code(
     submission: Result<web::Json<CodeSubmission>, actix_web::Error>,
+    counter: web::Data<MarkCounter>,
 ) -> Result<impl Responder, JudgeError> {
     let submission = submission.map_err(|e| JudgeError::PayloadParseError(e))?;
     tracing::info!(
@@ -21,6 +27,14 @@ async fn submit_code(
         submission.language
     );
 
+    let mut current_count = counter.count.load(Ordering::SeqCst);
+    while current_count >= MAX_SUBMISSIONS {
+        tracing::info!("Max submissions reached, waiting for a slot...");
+        tokio::time::sleep(Duration::from_secs(1)).await;
+        current_count = counter.count.load(Ordering::SeqCst);
+    }
+    let previous_count = counter.count.fetch_add(1, Ordering::SeqCst);
+    tracing::info!("Current count: {}", previous_count + 1);
     let output = AssertUnwindSafe(mark(MarkParams {
         language: &submission.language.clone().into(),
         code: &submission.code,
@@ -31,7 +45,11 @@ async fn submit_code(
     }))
     .catch_unwind()
     .await
-    .map_err(|_| JudgeError::HoduCoreError)?;
+    .map_err(|_| {
+        counter.count.fetch_sub(1, Ordering::SeqCst);
+        JudgeError::HoduCoreError
+    })?;
+    counter.count.fetch_sub(1, Ordering::SeqCst);
 
     Ok(web::Json(
         serde_json::to_value(MarkResponse::new(&output, &submission.fields)).unwrap(),
