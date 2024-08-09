4701bc3eadd97c0c56787dfc4f25d5d9a22d2128
MINKYU LEE <whizkyu@snu.ac.kr>
2024-08-09 23:18:00 +0900

    feat: 동시 채점 수 제한 (#45)
    ## 추가된 기능
    메모리가 터지는 것을 방지하기 위해 동시 채점 수를 제한합니다.
    
    ## 테스트 방법
    동시에 여러 요청을 날리며 log를 찍어봅니다.
    테스트를 위해 python의 aiohttp 라이브러리르 이용하여 동시에 요청하는 상황을 시뮬레이션해보았습니다.
    
    ## 관련 링크
    https://wafflestudio.slack.com/archives/C07DDPK0TD4/p1723207545909049

--- hodu-server/src/main.rs
+++ hodu-server/src/main.rs
@@ -1,13 +1,27 @@
-use actix_web::{App, HttpServer};
+use std::sync::atomic::AtomicU32;
+
+use actix_web::{web, App, HttpServer};
 
 mod api;
+
+pub struct MarkCounter {
+    count: AtomicU32,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     tracing_subscriber::fmt::init();
 
-    HttpServer::new(|| App::new().service(api::v1_router()))
-        .bind("0.0.0.0:8080")?
-        .run()
-        .await
+    let counter = web::Data::new(MarkCounter {
+        count: AtomicU32::new(0),
+    });
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(counter.clone())
+            .service(api::v1_router())
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
